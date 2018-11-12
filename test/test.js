f3894608fe069c0b3f2c9b37cdb276ad7516f0a5
hosein <hosein2398@gmail.com>
2018-11-12 11:13:56 +0330

    test: temp changes

--- test/test.js
+++ test/test.js
@@ -2,17 +2,20 @@ import test from 'ava';
 var assert = require('assert');
 var todo = require('../lib/2do');
 
-test('should return tasks', async t => {
-    var shouldBe = ["This file if only for tests", "how are you?"];
-    var retured = await todo.show2doFile({
-        fileName: "main.js"
-    }, () => {});
-    t.deepEqual(shouldBe, retured);
-});
 
 test('should return name of the files [list command]', async t => {
     var shouldBe = ["main.js", "somefile.py"];
     var retured = await todo.list2do({}, () => {}, true);
+    t.deepEqual(shouldBe, retured);
+});
+
+
+
+test('should return tasks', async t => {
+    var shouldBe = ["This file is only for tests", "how are you?"];
+    var retured = await todo.show2doFile({
+        fileName: "main.js"
+    }, () => {});
     t.deepEqual(shouldBe, retured);
 });
 
@@ -29,14 +32,17 @@ test('should add a todo to .json file [add command]', async t => {
 
 });
 
-test('should delete a todo in .json file [delete command command]', async t => {
-    var list = await todo.delete2do({
-        fileName: 'small.php'
-    }, () => {},true);
-    var list = await todo.list2do({}, () => {}, true);
-    //after adding now we hate to test if it's really added to file
-    var shouldBe = ["main.js", "somefile.py"];
-    console.log('sdf');
-    t.deepEqual(shouldBe, list);
 
-});
+// Working on this one
+
+// test('should delete a todo in .json file [delete command command]', async t => {
+//     var list = await todo.delete2do({
+//         fileName: 'small.php',
+//         todoNumber: 1
+//     }, () => {}, true);
+//     var list = await todo.list2do({}, () => {}, true);
+//     //after adding now we hate to test if it's really added to file
+//     var shouldBe = ["main.js", "somefile.py"];
+//     t.deepEqual(shouldBe, list);
+
+// });
